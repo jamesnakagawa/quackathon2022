59627202ccfd24f02b64314a01c35109bbc05302
Sahaj Singh <sahajsingh2313@gmail.com>
2022-02-12 10:43:33 -0800

    fix ducky command

--- bot/main.py
+++ bot/main.py
@@ -33,13 +33,14 @@ async def on_message(message):
         await message.channel.send('Quack!')
 
 
-@client.command(name='ducky', help='Loads a duck image')
+@client.command()
 async def ducky(ctx):
     API = "https://random-d.uk/api/v2/random"
     async with request("GET", API, headers={}) as response:
         if response.status == 200:
             buffer = await response.json()
             url = buffer['url']
+            await ctx.send(f"{ctx.author.mention} Here's your ducky!")
             await ctx.send(url)
         else:
             await ctx.send("Error getting image. API returned {}".format(response.status))
