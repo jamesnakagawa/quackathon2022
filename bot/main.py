4890822138f010c5e820f6e6292ff93cf0244c55
Aodren Auffredou-Heinicke <54121510+aodrenah@users.noreply.github.com>
2022-02-12 16:27:04 -0800

    added option to change spawn frequency

--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,7 @@
 from aiohttp import *
 from client import client, TOKEN
 
-from commands import general, quacks, direct_messages, battle, duckmon
+from commands import general, quacks, direct_messages, battle, duckmon, changesettings
 
 # Run
 client.run(TOKEN)
