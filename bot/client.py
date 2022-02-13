5a054b743967f388c4628f7a04fc058a69bdbc2f
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2022-02-12 19:20:21 -0800

    Query to database implementation

--- bot/client.py
+++ bot/client.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
 import os
+from bot.models import DuckType
+import models
+import random
 from aiohttp import *
 from dotenv import load_dotenv
 from models import Session
@@ -16,3 +19,21 @@ intents.reactions = True
 
 session = Session()
 client = commands.Bot(command_prefix='$', intents=intents)
+
+# Load ducks into database
+session.add_all([
+DuckType(name = "Giles", attack_coeff = random.randint(15, 30), defend_coeff = random.randint(5, 12)),
+DuckType(name = "Kelsie", attack_coeff = random.randint(15, 30), defend_coeff = random.randint(5, 12)),
+DuckType(name = "Reese", attack_coeff = random.randint(15, 30), defend_coeff = random.randint(5, 12)),
+DuckType(name = "Bertha", attack_coeff = random.randint(15, 30), defend_coeff = random.randint(5, 12)),
+DuckType(name = "Lily", attack_coeff = random.randint(15, 30), defend_coeff = random.randint(5, 12)),
+DuckType(name = "Shakir", attack_coeff = random.randint(15, 30), defend_coeff = random.randint(5, 12)),
+DuckType(name = "Geese", attack_coeff = random.randint(15, 30), defend_coeff = random.randint(5, 12)),
+DuckType(name = "Clifford", attack_coeff = random.randint(15, 30), defend_coeff = random.randint(5, 12)),
+DuckType(name = "Lilia", attack_coeff = random.randint(15, 30), defend_coeff = random.randint(5, 12))])
+
+#query_array = [duck_type1, duck_type2, duck_type3, duck_type4, duck_type5, duck_type6, duck_type7, duck_type8, duck_type9]
+
+session.query(DuckType).count(9)
+session.commit()
+
