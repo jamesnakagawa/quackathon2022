3c01fc746c4ac3c367e868c1857f5c87c6550794
Aodren Auffredou-Heinicke <54121510+aodrenah@users.noreply.github.com>
2022-02-12 16:47:40 -0800

    added command to see bot status and content of settings.json

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,6 @@
 from client import client, TOKEN
 
-from commands import general, quacks, direct_messages, battle, duckmon, changesettings, quacking
+from commands import general, quacks, direct_messages, battle, duckmon, changesettings, quacking, status
 
 # Run
 client.run(TOKEN)
