64bf0cd34838f2df61ac171e0471f8cbc03c6d5e
Lionel Song <99521745+Wowboylol@users.noreply.github.com>
2022-02-12 18:15:11 -0800

    Bugfix reaction.py

--- bot/commands/reaction.py
+++ bot/commands/reaction.py
@@ -4,7 +4,5 @@ from client import client
 @client.event
 async def on_reaction_add(reaction, user):
     channel = client.get_channel(942101706739699735)
-    check = 0
-    #reaction.message.author
     if user.bot == False:
         await channel.send('{}'.format(user.mention) + ' has caught the duck!')
