766c71beda1e4754ed34e3830deed2deabb5c4cd
Sahaj Singh <sahajsingh2313@gmail.com>
2022-02-12 16:21:18 -0800

    vc duck quacking

--- bot/commands/quacking.py
+++ bot/commands/quacking.py
@@ -1,46 +1,80 @@
 from client import *
+import asyncio
 import youtube_dl
+
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0'
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+        self.url = ""
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+        filename = data['title'] if stream else ytdl.prepare_filename(data)
+        return filename
 
 
 @client.command()
 async def play(ctx):
-    song_there = os.path.isfile("quack.mp3")
     try:
-        if song_there:
-            os.remove("quack.mp3")
-    except PermissionError:
-        await ctx.send("Wait for the ducks to finish or use the 'stop' command")
-        return
-
-    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='General')
-    await voiceChannel.connect()
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
-    }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        server = ctx.message.guild
+        voice_channel = server.voice_client
         url = 'https://www.youtube.com/watch?v=3DGdQ4gdqT4'
-        ydl.download([url])
-    for file in os.listdir("./"):
-        if file.endswith(".mp3"):
-            os.rename(file, "song.mp3")
-    voice.play(discord.FFmpegPCMAudio("song.mp3"))
-    ctx.send("Let the quacking commence!")
+        async with ctx.typing():
+            filename = await YTDLSource.from_url(url, loop=client.loop)
+            voice_channel.play(discord.FFmpegPCMAudio(
+                executable="ffmpeg.exe", source=filename))
+        await ctx.send('**Now playing:** {}'.format(filename))
+        ctx.send("Let the quacking commence!")
+    except:
+        await ctx.send("The duck is not connected to a voice channel.")
 
 
-@client.command()
-async def leave(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    if voice.is_connected():
-        await voice.disconnect()
+@client.command(name='join', help='Tells the duck to join the voice channel')
+async def join(ctx):
+    if not ctx.message.author.voice:
+        await ctx.send("{} is not connected to a voice channel".format(ctx.message.author.name))
+        return
     else:
-        await ctx.send("There are no ducks in voice channels.")
+        channel = ctx.message.author.voice.channel
+    await channel.connect()
+
+
+@client.command(name='leave', help='To make the duck leave the voice channel')
+async def leave(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_connected():
+        await voice_client.disconnect()
+    else:
+        await ctx.send("The duck is not connected to a voice channel.")
 
 
 @client.command()
@@ -48,9 +82,6 @@ async def pause(ctx):
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
     if voice.is_playing():
         voice.pause()
-        song_there = os.path.isfile("quack.mp3")
-        if song_there:
-            os.remove("quack.mp3")
     else:
         await ctx.send("Not quacking at the moment.")
 
