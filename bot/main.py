4b08fdadbfb902eab9efb866e1661ef9a8365d50
jamesnakagawa <me@james.computer>
2022-02-12 19:39:54 -0800

    Add battle system into main.py

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,6 @@
 from client import client, TOKEN
 
-from commands import general, quacks, direct_messages, duckmon, changesettings, quacking, status, reaction
+from commands import general, quacks, battle, direct_messages, duckmon, changesettings, quacking, status, reaction
 
 # Run
 client.run(TOKEN)
