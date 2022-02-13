d3d0f222386eefc00273cec413669c74005c4888
Sahaj Singh <sahajsingh2313@gmail.com>
2022-02-12 16:54:34 -0800

    fix

--- bot/commands/status.py
+++ bot/commands/status.py
@@ -1,10 +1,12 @@
 from client import *
 import json
 
+
 @client.command(name="status", description="prints out bot status and content of settings.json")
 async def status(ctx):
     json_file = open('bot/settings.json', 'r', encoding="utf-8")
     data = json.load(json_file)
     json_file.close()
-    embed = discord.Embed(title="Bot Status", description="data, color=discord.Color.red())
+    embed = discord.Embed(title="Bot Status",
+                          description="data", color=discord.Color.red())
     await ctx.send(embed=embed)
